2c175db1d343517f18dd7a5dd043ec72787d5881
Emile Ré <nemile.re@gmail.com>
2014-08-26 20:43:48 +0200

    master forgot variable

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var sass = require('node-sass');
 
 var app = express();
 var server = http.Server(app);
